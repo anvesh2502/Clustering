25a84ad762d765a9a88811809904723780163212
anvesh2502 <anveshrocks@yahoo.co.in>
2016-07-15 01:03:24 -0500

    Added the creation of the hierarchical tree

--- Clustering/clusters.py
+++ Clustering/clusters.py
@@ -10,7 +10,7 @@ def readfile(filename) :
     data=[]
 
     for line in lines[1:] :
-        p==line.strip().split('\t')
+        p=line.strip().split('\t')
         #first column in each row is the rownames
         rownames.append(p[0])
         data.append([float(x) for x in p[1:]])
@@ -32,7 +32,9 @@ def pearson(v1,v2) :
 
     #Sum of products
 
-    pSum=sum([v1[i]*v2[i] for i in range(0,len(v1))])
+    size=min(len(v1),len(v2))
+
+    pSum=sum([v1[i]*v2[i] for i in range(size)])
 
     #Calculate pearson score
 
@@ -42,3 +44,88 @@ def pearson(v1,v2) :
     if den==0 : return 0
 
     return 1.0-num/den
+
+
+# The class which is used to represent the hierarchical tree
+
+class bicluster :
+
+    def __init__(self,vec,left=None,right=None,distance=0.0,id=None) :
+
+        self.left=left
+        self.right=right
+        self.vec=vec
+        self.id=id
+        self.distance=distance
+
+
+
+
+def hcluster(rows,distance=pearson) :
+
+    distances={}
+    currentclustid=-1
+
+    #Initially,the clusters are just the rows
+
+    clust=[bicluster(rows[i],id=i) for i in range(len(rows))]
+
+    while len(clust)>1 :
+
+        lowestpair=(0,1)
+        closest=distance(clust[0].vec,clust[1].vec)
+
+
+        # loop through every pair looking for the smallest distance
+        for i in range(len(clust)) :
+            for j in range(i+1,len(clust)) :
+                #distances is the cache of distance calculations
+                if (clust[i].id,clust[j].id) not in distances :
+                    distances[(clust[i].id,clust[j].id)]=distance(clust[i].vec,clust[j].vec)
+                d=distances[(clust[i].id,clust[j].id)]
+
+                if d<closest :
+                    closest=d
+                    lowestpair=(i,j)
+
+        mergevec=[(clust[lowestpair[0]].vec[i]+clust[lowestpair[1]].vec[i])/2.0 for i in range(len(clust[0].vec))]
+
+        #create the new cluster
+
+        newcluster=bicluster(mergevec,left=clust[lowestpair[0]],right=clust[lowestpair[1]],distance=closest,id=currentclustid)
+
+        #cluster ids that weren't in the original set are negative
+        currentclustid-=1
+        del clust[lowestpair[1]]
+        del clust[lowestpair[0]]
+        clust.append(newcluster)
+    return clust[0]
+
+
+def printclust(clust,labels=None,n=0) :
+
+    #indent to make a hierarchy model
+
+    for i in range(n) : print ' ',
+
+    if clust.id<0 :
+        #negative id meas that this is a branch
+        print '-'
+    else :
+        #positive means that this is an endpoint
+        if labels==None : print clust.id
+        else : print labels[clust.id]
+
+    # now print the right and left branches
+
+    if clust.left!=None : printclust(clust.left,labels=labels,n=n+1)
+    if clust.right!=None : printclust(clust.right,labels=labels,n=n+1)
+
+
+
+
+
+blognames,words,data=readfile('blogdata.txt')
+clust=hcluster(data)
+
+printclust(clust,labels=blognames)
